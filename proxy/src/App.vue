ba49554c74e0290682d7845e277ff6cb896dac62
shub1nk <shub1nk@yandex.ru>
2018-08-30 00:33:31 +0500

    Инициализировал проект, подключил axios, получил данные

---
+++ proxy/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <p>Test</p>
+    <ul id="example-1">
+      <li v-for="item in proxy">
+    {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      proxy: null
+    };
+  },
+  computed: {
+    // proxy ()
+  },
+  mounted() {
+    axios
+      .get("https://proxyfordevelopers.com/api/proxies/?format=json")
+      .then(response => {
+        this.proxy = response.data;
+        console.log(this.proxy);
+      });
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ proxy/src/store/store.js
@@ -0,0 +1,28 @@
+import Vue from 'Vue';
+import Vuex from 'Vuex';
+
+const state = {
+  listProxy = []
+};
+
+const mutation = {};
+
+const action = {
+
+};
+
+const getters = {
+
+}
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+	state,
+	mutations,
+	actions,
+	getters
+});
+
+export default store;
+ 
