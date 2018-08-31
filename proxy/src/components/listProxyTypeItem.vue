7997e699ae202934111faf5eca3729071281d127
Nikolay Shubin <shubinna@ventprom.com>
2018-08-31 11:34:07 +0500

    Добавил подсвечивание активного пункта

--- proxy/src/components/listProxyTypeItem.vue
+++ proxy/src/components/listProxyTypeItem.vue
@@ -3,9 +3,8 @@
     <h4 v-if="proxyCountryCounter > 0">proxy_type по убыванию</h4>
     <li
       v-for="itemProxy in proxyType" :key="itemProxy.id"
-      class="list-group-item"
-      :class="{active: isActive}"
-      @click="viewCurrentProxy(itemProxy.id)"
+      :class="{active: itemProxy.isActive, 'list-group-item': true}"
+      @click="viewCurrentProxy(itemProxy, $event)"
       >
         id-{{ itemProxy.id }}. host: {{itemProxy.host}} proxy_type = {{itemProxy.proxy_type}}
     </li>  
@@ -13,9 +12,8 @@
     <h4 v-if="proxyCountryCounter > 0">alive == true</h4>
     <li
       v-for="item in alive" :key="item.id"
-      class="list-group-item"
-      :class="{active: isActive}"
-      @click="viewCurrentProxy(item.id)"
+      :class="{active: item.isActive, 'list-group-item': true}"
+      @click="viewCurrentProxy(item, $event)"
       >
         id-{{ item.id }}. host: {{item.host}} alive = {{item.alive}}
     </li>
@@ -27,16 +25,11 @@ import { mapGetters } from "vuex";
 
 export default {
   name: "listProxyTypeItem",
-  data() {
-    return {
-      isActive: false
-    };
-  },
   created() {
     this.$store.dispatch("getProxyList");
   },
   computed: {
-    ...mapGetters(["proxyCountry", 'proxyCountryCounter']),
+    ...mapGetters(["proxyCountry", "proxyCountryCounter"]),
     proxyType() {
       let proxyType2 = this.proxyCountry.filter(item => item.proxy_type === 2);
       let proxyType1 = this.proxyCountry.filter(item => item.proxy_type === 1);
@@ -52,11 +45,11 @@ export default {
     }
   },
   methods: {
-    viewCurrentProxy(id, e) {
-      // this.isActive = !this.isActive;
-      // alert(e.target);
-      // e.target.className = this.isActive ? "active" : "";
-      this.$store.dispatch("setProxyInfo", id);
+    viewCurrentProxy(itemProxy, e) {
+      console.log(e);
+      document.querySelectorAll('li').forEach(item => item.classList.remove('active'));
+      e.target.classList.toggle('active');
+      this.$store.dispatch("setProxyInfo", itemProxy.id);
     }
   }
 };
